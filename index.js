d9becf62b94d70aa267f16699869d9836957b87c
karanpatel <kpatel74155@gmail.com>
2024-05-14 16:31:47 +0530

    add testing

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json()); 
 app.use(morgan("dev"))
 app.get("/",(req,res)=>{
+    console.log("Hello me yaha par hu")
     res.send("<h1>this is for testing</h1>")
 })
 

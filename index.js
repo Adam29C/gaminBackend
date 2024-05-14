d033ea8a000d684d32ac1c4b84c1d79b42272997
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-05-14 15:38:00 -0400

    add morgan package

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const express = require('express');
 const bodyParser = require('body-parser'); 
 const cors = require('cors');
 const route = require('./src/router');
-const connection =require("./src/connection/db")
+const connection =require("./src/connection/db");
+const morgan = require('morgan');
 const app = express(); 
 
 // Middleware setup
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false })); 
 app.use(bodyParser.json()); 
-
+app.use(morgan("dev"))
 // Define routes
 app.use('/api/v1', route); 
 

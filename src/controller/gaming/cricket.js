53dcec915b5cf9b04a652fd4f475b8aba6e73bca
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-05-16 14:58:38 -0400

    create gaming api

--- src/controller/gaming/cricket.js
+++ src/controller/gaming/cricket.js
@@ -18,30 +18,44 @@ const getAllMatchesList = async (req, res) => {
         });
     }
 }
-// const getSeriesList = async (req, res) => {
-//     try {
+const getSeriesList = async (req, res) => {
+    try {
+        const response = await axios.get(`https://rest.entitysport.com/exchange/competitions?token=${match_token}&status=live`);
+        const allMatchesList = response.data;
+        return res.status(200).send({
+            statusCode: 200,
+            status: "success",
+            data: allMatchesList
+        });
+    } catch (error) {
+        return res.status(500).send({
+            statusCode: 500,
+            status: "failure",
+            msg: "Internal Server Error"
+        });
+    }
+}
 
-//     } catch (error) {
-//         return res.status(500).send({
-//             statusCode: 500,
-//             status: "failure",
-//             msg: "Internal Server Error"
-//         });
-//     }
-// }
-
-// const getMatchsList = async (req, res) => {
-//     try {
-
-//     } catch (error) {
-//         return res.status(500).send({
-//             statusCode: 500,
-//             status: "failure",
-//             msg: "Internal Server Error"
-//         });
-//     }
-// }
+const getMatchsList = async (req, res) => {
+    try {
+        const {seriesId}=req.params;
+        if(!seriesId){
+            return res.status(400).send({
+                statusCode: 400,
+                status: "success",
+                data: allMatchesList
+            });
+        }
+    } catch (error) {
+        return res.status(500).send({
+            statusCode: 500,
+            status: "failure",
+            msg: "Internal Server Error"
+        });
+    }
+}
 
 module.exports={
-    getAllMatchesList
+    getAllMatchesList,
+    getSeriesList
 }
--- src/router/gaming.js
+++ src/router/gaming.js
@@ -2,8 +2,9 @@ let express = require('express')
 let app = express()
 // const controller = require('../controller/admin')
 const { authenticateToken } = require('../helper/middleware');
-const { getAllMatchesList } = require('../controller/gaming/cricket');
+const { getAllMatchesList, getSeriesList } = require('../controller/gaming/cricket');
 
-app.get('/getAllMatchesList',authenticateToken,getAllMatchesList)
+app.get('/getAllMatchesList',authenticateToken,getAllMatchesList);
+app.get("/getSeriesList",authenticateToken,getSeriesList)
  
 module.exports = app

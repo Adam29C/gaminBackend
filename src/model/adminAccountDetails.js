ae676129c1c9541fbfe79ff1e31f9d2aedef0e72
karanpatel <kpatel74155@gmail.com>
2024-05-23 18:43:32 +0530

    add code

---
+++ src/model/adminAccountDetails.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const accountDetailsSchema = new Schema({
+    adminId: { type: String },
+    bank: [{
+        accountNumber: { type: String, required: true },
+        accountHolderName: { type: String, required: true },
+        ifscCode: { type: String, required: true },
+        bankName: { type: String, required: true },
+        isBank: { type: String, require: true },
+        bankImage: { type: String }
+    }],
+    upi: [{
+        upiId: { type: String, required: true },
+        upiName: { type: String, required: true },
+        isBank: { type: String, require: true },
+        barCodeImage: { type: String, require: true }
+    }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model('adminAccountDetail', accountDetailsSchema)

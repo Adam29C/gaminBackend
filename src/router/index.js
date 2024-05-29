9cbcc33f1cc32e569abab1d9f5ea62775a606c9f
Adam29C <adamcatholic29@gmail.com>
2024-05-29 17:05:40 +0530

    Revert "Webhook"

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,11 @@
 const express = require('express') 
 const router = express.Router()
 const gaming = require('./gaming');
-const webData = require("./web")
 router.use('/userRouter', require('./userRouter'))
 router.use('/adminRouter', require('./adminRouter'))
 router.use('/subAdminRouter', require('./subAdminRouter'))
 router.use('/authRouter', require('./authRouter'));
 router.use("/common",require('./common'))
-router.use('/sports',gaming);
-router.use("/webData",webData)
+router.use('/sports',gaming)
 
 module.exports = router

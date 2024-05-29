2db22c70d99250f729f3ec25d4ec804c3da61cc5
karanpatel <kpatel74155@gmail.com>
2024-05-29 18:22:16 +0530

    add code

--- src/controller/gaming/bids.js
+++ src/controller/gaming/bids.js
@@ -3,7 +3,22 @@ const Users = require("../../model/user");
 
 const createBids = async (req, res) => {
     try {
-         const { userId, bidAmount, bidParsent, teamid, teamName, selectOptionName,} = req.body
+        const { userId, bidAmount, bidParsent, teamid, teamName, batingType, batingOption, fancyid } = req.body;
+        if (!userId) {
+            return res.status(400).send({
+                statusCode: 400,
+                status: "failure",
+                message: "series id required"
+            });
+        }
+        let userDetails = await Users.findOne({_id:userId});
+        if(!userDetails){
+
+        }
+        let totalAmount=0;
+        if(batingOption==="matchodds" || batingOption==="tiedmatch" || batingOption ==="bookmaker"){
+          totalAmount
+        }
     } catch (error) {
         return res.status(500).send({
             status: "failure",

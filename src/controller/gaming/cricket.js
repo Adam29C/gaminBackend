b3412be38a2bcb192008a4390737ef3710740ca0
karanpatel <kpatel74155@gmail.com>
2024-05-29 18:28:38 +0530

    add code

--- src/controller/gaming/cricket.js
+++ src/controller/gaming/cricket.js
@@ -88,9 +88,36 @@ const matchDetails =async(req,res)=>{
         });
     }
 }
+const getMatchScore=async(req,res)=>{
+    try{
+        const {matchId}=req.params;
+        if(!matchId){
+            return res.status(400).send({
+                statusCode: 400,
+                status: "failure",
+                message: "Match id required"
+            });
+        }
+        const response = await axios.get(`https://rest.entitysport.com/exchange/matches/${matchId}/innings/1/commentary?token=${match_token}`);
+        const allMatchesList = response.data;
+        return res.status(200).send({
+            statusCode: 200,
+            status: "success",
+            data: allMatchesList
+        });
+    }catch (error) {
+        return res.status(500).send({
+            statusCode: 500,
+            status: "failure",
+            msg: messages.failure
+        });
+    }
+}
+
 module.exports={
     getAllMatchesList,
     getSeriesList,
     getMatchList,
     matchDetails,
+    getMatchScore,
 }
--- src/router/gaming.js
+++ src/router/gaming.js
@@ -1,10 +1,11 @@
 let express = require('express')
 let app = express();
 const { authenticateToken } = require('../helper/middleware');
-const { getAllMatchesList, getSeriesList, getMatchList, matchDetails } = require('../controller/gaming/cricket');
+const { getAllMatchesList, getSeriesList, getMatchList, matchDetails, getMatchScore } = require('../controller/gaming/cricket');
 
 app.get('/getAllMatchesList', authenticateToken, getAllMatchesList);
-app.get("/getSeriesList", authenticateToken, getSeriesList)
+app.get("/getSeriesList", authenticateToken, getSeriesList);
 app.get("/getMatchList/:seriesId", authenticateToken, getMatchList);
-app.get("/matchDetails/:matchId",authenticateToken,matchDetails)
+app.get("/matchDetails/:matchId", authenticateToken, matchDetails);
+app.get("/getMatchScore", authenticateToken, getMatchScore);
 module.exports = app

2362410bc4292781dffac96e2d5f6879bc8802e4
aayush22200 <168806480+aayush22200@users.noreply.github.com>
2024-06-04 18:58:46 -0400

    add the code

--- src/controller/admin/adminDashboard.js
+++ src/controller/admin/adminDashboard.js
@@ -1385,6 +1385,22 @@ const transectionAndBankingList = async (req, res) => {
             userTransectionInfo.push({ _id: info._id, userId: info.userId, name: a.name, mobile: a.mobileNumber })
 
         }
+
+        // Extracting required fields from bank and upi arrays
+        const extractedBankInfo = adminAccountInfo.map(admin => ({
+            _id: admin._id,
+            bank: admin.bank.map(bank => ({
+                accountNumber: bank.accountNumber,
+                accountHolderName: bank.accountHolderName,
+                _id: bank._id
+            })),
+            upi: admin.upi.map(upi => ({
+                upiId: upi.upiId,
+                upiName: upi.upiName,
+                _id: upi._id
+            }))
+        }));
+
         // Sorting logic
         if (sortBy) {
             const order = sortOrder && sortOrder.toLowerCase() === 'desc' ? -1 : 1;
@@ -1395,7 +1411,7 @@ const transectionAndBankingList = async (req, res) => {
             });
         }
 
-        const data = { adminAccountInfo, userTransectionInfo };
+        const data = { adminAccountInfo: extractedBankInfo, userTransectionInfo };
 
         return res.status(200).send({
             statusCode: 200,
@@ -1412,6 +1428,8 @@ const transectionAndBankingList = async (req, res) => {
         });
     }
 };
+
+
 
 const transectionDetailsBankingById = async (req, res) => {
     try {
